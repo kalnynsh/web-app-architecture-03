2712dd7a15afc2a5184d291bd811ecdcea537129
kalnynsh <kda869@yandex.ru>
2018-11-14 11:48:21 +0300

    Start project

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Controller;
+
+use Framework\Render;
+use Service\Order\Basket;
+use Service\Product\Product;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController
+{
+    use Render;
+
+    /**
+     * Информация о продукте
+     *
+     * @param Request $request
+     * @param string $id
+     * @return Response
+     */
+    public function infoAction(Request $request, $id): Response
+    {
+        $basket = (new Basket($request->getSession()));
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $basket->addProduct((int)$request->request->get('product'));
+        }
+
+        $productInfo = (new Product())->getInfo((int)$id);
+
+        if ($productInfo === null) {
+            return $this->render('error404.html.php');
+        }
+
+        $isInBasket = $basket->isProductInBasket($productInfo->getId());
+
+        return $this->render('product/info.html.php', ['productInfo' => $productInfo, 'isInBasket' => $isInBasket]);
+    }
+
+    /**
+     * Список всех продуктов
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function listAction(Request $request): Response
+    {
+        $productList = (new Product())->getAll();
+
+        // Применить паттерн Стратегия
+        // $request->query->get('price'); // Сортировка по цене
+        // $request->query->get('name'); // Сортировка по имени
+
+        return $this->render('product/list.html.php', ['productList' => $productList]);
+    }
+}
---
+++ src/Service/Product/Product.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Service\Product;
+
+use Model;
+
+class Product
+{
+    /**
+     * Получаем информацию по конкретному продукту
+     *
+     * @param int $id
+     * @return Model\Entity\Product|null
+     */
+    public function getInfo(int $id): ?Model\Entity\Product
+    {
+        $product = $this->getProductRepository()->search([$id]);
+        return count($product) ? $product[0] : null;
+    }
+
+    /**
+     * Получаем все продукты
+     *
+     * @return Model\Entity\Product[]
+     */
+    public function getAll(): array
+    {
+        return $this->getProductRepository()->fetchAll();
+    }
+
+    /**
+     * Фабричный метод для репозитория Product
+     *
+     * @return Model\Repository\Product
+     */
+    protected function getProductRepository(): Model\Repository\Product
+    {
+        return new Model\Repository\Product();
+    }
+}
